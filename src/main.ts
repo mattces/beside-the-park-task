b73d8efea676d114e20d210434f5d6647e3e79f4
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-24 21:58:02 +0100

    Add dotenv config to main.ts
    This is so env variables are available form start (before AppModule is initialized) -- a call to dotenvConfig is also present in typeorm.ts, because this module will be used as a --dataSource script by the typeorm CLI (in this case main.ts is not ran)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {config as dotenvConfig} from "dotenv";
+
+
+if (process.env.NODE_ENV == "development") {
+  dotenvConfig({ path: ".env.development.local" });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
