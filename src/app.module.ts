d55848320faccac14110260d633a464f02fe1582
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-24 21:47:36 +0100

    Integrate typeorm and a pg db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import typeorm from "./model/typeorm/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // define the environment variables in docker-compose for production
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync(
+      {
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => configService.get("typeorm"),
+      },
+      ), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/model/typeorm/typeorm.ts
@@ -0,0 +1,24 @@
+import { registerAs } from "@nestjs/config";
+import { config as dotenvConfig } from "dotenv";
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as process from "process";
+
+
+dotenvConfig({ path: ".env.development.local" });
+
+
+const config = {
+  type: "postgres",
+  host: `${process.env.DB_HOST}`,
+  port: `${process.env.DB_PORT}`,
+  username: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_NAME}`,
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  migrations: ["dist/migrations/*{.ts,.js}"],
+  autoLoadEntities: true,
+  synchronize: process.env.NODE_ENV === "development"
+};
+
+export default registerAs("typeorm", () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
