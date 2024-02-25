e5baa795048a370badeb09cb3ce4ab87befd72f5
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-25 22:15:47 +0100

    Import database module into quiz module

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { QuizResolver } from './resolvers/quiz.resolver';
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   providers: [QuizResolver]
 })
 export class QuizModule {}
