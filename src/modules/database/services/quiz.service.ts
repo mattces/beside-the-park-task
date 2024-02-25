69a65d38243ee97d6e86cbbc3165456970a00d21
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-25 22:16:56 +0100

    Add method to create quiz with all subobjects in one transaction

--- src/modules/database/services/quiz.service.ts
+++ src/modules/database/services/quiz.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
+import { InjectDataSource, InjectRepository } from "@nestjs/typeorm";
 import { Quiz } from "../model/quiz.entity";
-import { DeepPartial, Repository, TypeORMError } from "typeorm";
+import { DataSource, DeepPartial, QueryRunner, Repository, TypeORMError } from "typeorm";
+import { CreateQuizInput } from "../../quiz/resolvers/quiz.input";
+import { Answer } from "../model/answer.entity";
+import { Question } from "../model/question.entity";
+import { AnswerService } from "./answer.service";
+import { QuizModule } from "../../quiz/quiz.module";
 
 @Injectable()
 export class QuizService {
+
+  private queryRunner: QueryRunner;
+
   constructor(
     @InjectRepository(Quiz)
-    private readonly quizRepository: Repository<Quiz>
+    private readonly quizRepository: Repository<Quiz>,
+    @InjectDataSource()
+    private readonly dataSource: DataSource
   ) {
+    this.queryRunner = dataSource.createQueryRunner();
   }
 
   async findAll(): Promise<Quiz[]> {
@@ -27,21 +38,47 @@ export class QuizService {
     }
   }
 
-  create(quizObject: Omit<Quiz, "id" | "questions">): Quiz {
+  async create(quizInput: CreateQuizInput): Promise<Quiz> {
     try {
-      return this.quizRepository.create(quizObject);
+      await this.queryRunner.startTransaction();
+
+      const newQuiz = this.queryRunner.manager.create(Quiz, quizInput);
+      await this.queryRunner.manager.save(Quiz, newQuiz);
+
+      const newQuestions: Question[] = [];
+
+      for (const questionInput of quizInput.questions) {
+
+        const newQuestion = this.queryRunner.manager.create(Question, { ...questionInput, quiz: newQuiz });
+        await this.queryRunner.manager.save(Question, newQuestion);
+
+        const newAnswers: Answer[] = [];
+        for (const answerInput of questionInput.answers) {
+          const newAnswer = this.queryRunner.manager.create(Answer, { ...answerInput, question: newQuestion });
+          await this.queryRunner.manager.save(Answer, newAnswer);
+
+          newAnswers.push(newAnswer);
+        }
+        newQuestion.answers = newAnswers;
+        newQuestions.push(newQuestion);
+      }
+
+      await this.queryRunner.commitTransaction();
+
+      newQuiz.questions = newQuestions;
+      return newQuiz;
     } catch (e) {
-      throw new TypeORMError(`Failed to create new question (${JSON.stringify(quizObject)}): ${e.message}`);
+      await this.queryRunner.rollbackTransaction();
+      throw new TypeORMError(`Failed to create new quiz (${JSON.stringify(quizInput)}): ${e.message}`);
     }
   }
 
   async update(id: string, quizObject: DeepPartial<Omit<Quiz, "id" | "questions">>): Promise<Quiz> {
     try {
       await this.quizRepository.update(id, quizObject);
-      /**A find one method will never be needed in this service.*/
-      return await this.quizRepository.findOneBy({ id });
+      return await this.findOneById(id);
     } catch (e) {
-      throw new TypeORMError(`Failed to update question (ID ${id}, ${JSON.stringify(quizObject)}): ${e.message}`);
+      throw new TypeORMError(`Failed to update quiz (ID ${id}, ${JSON.stringify(quizObject)}): ${e.message}`);
     }
   }
 
@@ -49,7 +86,7 @@ export class QuizService {
     try {
       await this.quizRepository.delete(id);
     } catch (e) {
-      throw new TypeORMError(`Failed to delete question (ID ${id}): ${e.message}`);
+      throw new TypeORMError(`Failed to delete quiz (ID ${id}): ${e.message}`);
     }
   }
 }
