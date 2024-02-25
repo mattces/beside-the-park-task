106e774101227b1bd173f066f0809be2b2f6d745
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-25 14:29:07 +0100

    Add queries to QuizResolver
    The queries are empty for now, added just so the development server can run.

--- src/resolvers/quiz.resolver.ts
+++ src/resolvers/quiz.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, ID, Resolver, Query } from "@nestjs/graphql";
+import { Quiz } from "../modules/database/services/quiz/quiz.entity";
 
-@Resolver('Quiz')
-export class QuizResolver {}
+@Resolver(of => Quiz)
+export class QuizResolver {
+  @Query(returns => Quiz, {nullable: true})
+  async quiz(@Args('id', { type: () => ID }) id: string): Promise<Quiz | null> {
+    return null 
+  }
+  @Query(returns => [Quiz])
+  async quizzes() {
+    return [] as Quiz[]
+  }
+}
