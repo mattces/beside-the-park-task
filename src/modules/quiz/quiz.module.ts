bb07530ffde86536631df5c49b296175092c637c
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-25 22:27:47 +0100

    Add question resolver

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuizResolver } from './resolvers/quiz.resolver';
 import { DatabaseModule } from "../database/database.module";
+import { QuestionResolver } from './resolvers/question.resolver';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [QuizResolver]
+  providers: [QuizResolver, QuestionResolver]
 })
 export class QuizModule {}
---
+++ src/modules/quiz/resolvers/question.resolver.ts
@@ -0,0 +1,18 @@
+import { Parent, ResolveField, Resolver } from "@nestjs/graphql";
+import { Question } from "../../database/model/question.entity";
+import { AnswerService } from "../../database/services/answer.service";
+
+@Resolver(of => Question)
+export class QuestionResolver {
+  constructor(
+    private readonly answerService: AnswerService
+  ) {
+  }
+  @ResolveField()
+  async answers(@Parent() question: Question){
+    if (question.answers) {
+      return question.answers;
+    }
+    return this.answerService.findAll(question.id);
+  }
+}
