6203dfc6048452187f14ad889178cb86e09f4150
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-25 22:17:39 +0100

    Add queries, create mutation and questions resolution

--- src/modules/quiz/resolvers/quiz.resolver.ts
+++ src/modules/quiz/resolvers/quiz.resolver.ts
@@ -1,14 +1,40 @@
-import { Args, ID, Resolver, Query } from "@nestjs/graphql";
+import { Args, ID, Resolver, Query, Mutation, ResolveField, Parent } from "@nestjs/graphql";
 import { Quiz } from "../../database/model/quiz.entity";
+import { QuizService } from "../../database/services/quiz.service";
+import { CreateQuizInput } from "./quiz.input";
+import { QuestionService } from "../../database/services/question.service";
+
 
 @Resolver(of => Quiz)
 export class QuizResolver {
+  constructor(
+    private readonly quizService: QuizService,
+    private readonly questionService: QuestionService
+  ) {
+  }
+
   @Query(returns => Quiz, {nullable: true})
   async quiz(@Args('id', { type: () => ID }) id: string): Promise<Quiz | null> {
-    return null 
+    return this.quizService.findOneById(id); 
   }
   @Query(returns => [Quiz])
   async quizzes() {
-    return [] as Quiz[]
+    return this.quizService.findAll()
+  }
+  
+  @Mutation(returns => Quiz) 
+  async createQuiz(
+    @Args('quiz', {type: () => CreateQuizInput}) 
+    quiz: CreateQuizInput
+  ) {
+   return this.quizService.create(quiz);
+  }
+  
+  @ResolveField()
+  async questions(@Parent() quiz: Quiz) {
+    if (quiz.questions) {
+      return quiz.questions;
+    }
+    return this.questionService.findAll(quiz.id); 
   }
 }
