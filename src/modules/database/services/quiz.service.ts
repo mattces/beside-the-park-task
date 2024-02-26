81736eabc28e7e15ab7d6970b3c6ff362d71e9f2
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-26 09:16:05 +0100

    Add HttpExceptions (400) on invalid data in createQuiz

--- src/modules/database/services/quiz.service.ts
+++ src/modules/database/services/quiz.service.ts
@@ -1,12 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectDataSource, InjectRepository } from "@nestjs/typeorm";
 import { Quiz } from "../model/quiz.entity";
 import { DataSource, DeepPartial, QueryRunner, Repository, TypeORMError } from "typeorm";
 import { CreateQuizInput } from "../../quiz/resolvers/quiz.input";
 import { Answer } from "../model/answer.entity";
-import { Question } from "../model/question.entity";
-import { AnswerService } from "./answer.service";
-import { QuizModule } from "../../quiz/quiz.module";
+import { Question, QuestionType } from "../model/question.entity";
 
 @Injectable()
 export class QuizService {
@@ -47,14 +45,44 @@ export class QuizService {
 
       const questions: Question[] = [];
       let questionOrder = 0;
+      
+      if (quizInput.questions.length == 0) {
+        throw new HttpException(`Quiz must have at least one question. (${JSON.stringify(quizInput)})`, HttpStatus.BAD_REQUEST);
+      }
 
+      
       for (const questionInput of quizInput.questions) {
-
+        if (questionInput.type == QuestionType.OpenEnded) {
+          if (questionInput.answers.length != 1) {
+            throw new HttpException(`"OpenEnded" type question must have exactly one answer. (${JSON.stringify(questionInput)}).`, HttpStatus.BAD_REQUEST);
+          }
+        } else if (questionInput.answers.length <= 2) {
+          throw new HttpException(`Question must have at least two answers. (${JSON.stringify(questionInput)}).`, HttpStatus.BAD_REQUEST);
+        }
+        
+        const correctAnswersCount = questionInput.answers.filter(ans => ans.correct).length;
+        
+        if (correctAnswersCount == 0 && questionInput.type ==  QuestionType.MultipleChoice) {
+          throw new HttpException(`"MultipleChoice" type question must have at least one correct answer. (${JSON.stringify(questionInput)}).`, HttpStatus.BAD_REQUEST);
+        }
+        if (correctAnswersCount != 1 && questionInput.type ==  QuestionType.SingleChoice) {
+          throw new HttpException(`"SingleChoice" type question must have at exactly one correct answer. (${JSON.stringify(questionInput)}).`, HttpStatus.BAD_REQUEST);
+        }
+        
         const question = this.queryRunner.manager.create(Question, { ...questionInput, quiz: quiz,  order: questionOrder  });
         await this.queryRunner.manager.save(Question, question);
+        
 
         const answers: Answer[] = [];
         for (const answerInput of questionInput.answers) {
+          if (questionInput.type == QuestionType.OpenEnded) {
+            if (answerInput.correct != null || answerInput.order == null) {
+              throw new HttpException(`"OpenEnded" type question answers cannot have 'correct', 'order' properties. (${JSON.stringify(answerInput)}).`, HttpStatus.BAD_REQUEST);
+            }
+          }
+          else if (answerInput.correct != null && answerInput.order != null) {
+            throw new HttpException(`Answer cannot have both 'order' and 'correct' properties. (${JSON.stringify(answerInput)}).`, HttpStatus.BAD_REQUEST);
+          } 
           
           const answer = this.queryRunner.manager.create(Answer, { ...answerInput, question: question});
           await this.queryRunner.manager.save(Answer, answer);
@@ -73,7 +101,12 @@ export class QuizService {
       return quiz;
     } catch (e) {
       await this.queryRunner.rollbackTransaction();
-      throw new TypeORMError(`Failed to create new quiz (${JSON.stringify(quizInput)}): ${e.message}`);
+      if (e instanceof HttpException) {
+        throw e;
+      }
+      else {
+        throw new TypeORMError(`Failed to create new quiz (${JSON.stringify(quizInput)}): ${e.message}`);
+      }
     }
   }
 
