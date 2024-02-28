5bd620b7be61d0f6bba1770c4d1b982622eaced6
Mateusz Cieśla <mciesla09@gmail.com>
2024-02-28 12:54:21 +0100

    LATE FIX: Add versioned .env.db for convenience when setting up

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -9,11 +9,11 @@ if (process.env.NODE_ENV == "development") {
 
 const config = {
   type: "postgres",
-  host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
-  username: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`,
+  host: `${process.env.POSTGRES_HOST}`,
+  port: `${process.env.POSTGRES_PORT}`,
+  username: `${process.env.POSTGRES_USER}`,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: `${process.env.POSTGRES_NAME}`,
   entities: ["dist/**/*.entity{.ts,.js}"],
   migrations: ["dist/migrations/*{.ts,.js}"],
   autoLoadEntities: true,
